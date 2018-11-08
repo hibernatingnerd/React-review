7097655b9352720784cbcee7e593c6fe84f71875
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-11-08 00:03:06 -0500

    add router

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,14 +1,25 @@
 import React, { Fragment } from 'react';
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    console.log('Going to store');
+
+  }
   render() {
     return (
       <Fragment>
         <p>Fish!</p>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please Enter a Store</h2>
-          <input type="text" required placeholder="Store Name" />
-          <button type="submit">Visit Store</button>
+          <input
+            type="text"
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button  type="submit">Visit Store</button>
         </form>
       </Fragment>
     )
